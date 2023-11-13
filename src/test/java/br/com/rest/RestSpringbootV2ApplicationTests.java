634d288808db073178db1bfbac4a42135e9ad64d
lucas <lucasmicco@hotmail.com>
2023-11-13 20:15:11 -0300

    Initial project files

---
+++ src/test/java/br/com/rest/RestSpringbootV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringbootV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
