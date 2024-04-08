3e72cde91d45da254da357de152bee3d6ce03501
lucas <lucasmicco@hotmail.com>
2024-04-08 20:48:09 -0300

    Adicionando documentação de respostas da API pela anotação ApiResponse e adicionando classe estatica String Helper

--- src/main/java/br/com/rest/Controller/PersonController.java
+++ src/main/java/br/com/rest/Controller/PersonController.java
@@ -2,7 +2,9 @@ package br.com.rest.Controller;
 
 import br.com.rest.Domain.Dtos.V1.PersonDto;
 import br.com.rest.Domain.Dtos.V2.PersonV2Dto;
+import br.com.rest.Helper.StringHelper;
 import br.com.rest.Services.Services.PersonService;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
@@ -19,18 +21,25 @@ public class PersonController {
     private PersonService personService;
 
     @GetMapping(value = "/personName/{id}")
+    @ApiResponse(description = StringHelper.Ok, responseCode = StringHelper.Ok_STATUS_CODE_VALUE, content = @Content)
+    @ApiResponse(description = StringHelper.NOT_FOUND, responseCode = StringHelper.NOT_FOUND_STATUS_CODE_VALUE, content = @Content)
+    @ApiResponse(description = StringHelper.INTERNAL_SERVER_ERROR, responseCode = StringHelper.INTERNAL_SERVER_ERROR_STATUS_CODE_VALUE, content = @Content)
     public String getPersonName(@PathVariable("id") Long id){
         return personService.getPersonName(id);
     }
 
     @GetMapping(value = "/findById/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ApiResponse(responseCode = "200")
+    @ApiResponse(description = StringHelper.Ok, responseCode = StringHelper.Ok_STATUS_CODE_VALUE, content = @Content)
+    @ApiResponse(description = StringHelper.NOT_FOUND, responseCode = StringHelper.NOT_FOUND_STATUS_CODE_VALUE, content = @Content)
+    @ApiResponse(description = StringHelper.INTERNAL_SERVER_ERROR, responseCode = StringHelper.INTERNAL_SERVER_ERROR_STATUS_CODE_VALUE, content = @Content)
     public PersonDto retrievePersonById(@PathVariable("id") String id){
         return personService.findById(id);
     }
 
     @GetMapping(value = "/findAllPersons", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ApiResponse(responseCode = "200")
+    @ApiResponse(description = StringHelper.Ok, responseCode = StringHelper.Ok_STATUS_CODE_VALUE, content = @Content)
+    @ApiResponse(description = StringHelper.NOT_FOUND, responseCode = StringHelper.NOT_FOUND_STATUS_CODE_VALUE, content = @Content)
+    @ApiResponse(description = StringHelper.INTERNAL_SERVER_ERROR, responseCode = StringHelper.INTERNAL_SERVER_ERROR_STATUS_CODE_VALUE, content = @Content)
     public List<PersonDto> retrieveAllPersons(){
         return personService.findAll();
     }
@@ -39,21 +48,25 @@ public class PersonController {
             value = "/createPerson",
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ApiResponse(responseCode = "201")
+    @ApiResponse(description = StringHelper.CREATED, responseCode = StringHelper.CREATED_STATUS_CODE_VALUE, content = @Content)
+    @ApiResponse(description = StringHelper.INTERNAL_SERVER_ERROR, responseCode = StringHelper.INTERNAL_SERVER_ERROR_STATUS_CODE_VALUE, content = @Content)
     public PersonDto createPerson(@RequestBody(required = true) PersonDto person){
         return personService.create(person);
     }
 
     @PostMapping(value = "/createPerson/v2", produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ApiResponse(responseCode = "201")
+    @ApiResponse(description = StringHelper.CREATED, responseCode = StringHelper.CREATED_STATUS_CODE_VALUE, content = @Content)
+    @ApiResponse(description = StringHelper.INTERNAL_SERVER_ERROR, responseCode = StringHelper.INTERNAL_SERVER_ERROR_STATUS_CODE_VALUE, content = @Content)
     public PersonV2Dto createPerson(@RequestBody(required = true) PersonV2Dto person){
         return personService.newCreatePerson(person);
     }
 
     @PutMapping(value = "/updatePerson/{id}", produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ApiResponse(responseCode = "200")
+    @ApiResponse(description = StringHelper.Ok, responseCode = StringHelper.Ok_STATUS_CODE_VALUE, content = @Content)
+    @ApiResponse(description = StringHelper.BAD_REQUEST, responseCode = StringHelper.BAD_REQUEST_STATUS_CODE_VALUE, content = @Content)
+    @ApiResponse(description = StringHelper.INTERNAL_SERVER_ERROR, responseCode = StringHelper.INTERNAL_SERVER_ERROR_STATUS_CODE_VALUE, content = @Content)
     public PersonDto updatePerson(
             @PathVariable(value = "id") Long id,
             @RequestBody(required = true) PersonDto person){
@@ -61,7 +74,9 @@ public class PersonController {
     }
 
     @DeleteMapping("/deletePerson/{id}")
-    @ApiResponse(responseCode = "200")
+    @ApiResponse(description = StringHelper.Ok, responseCode = StringHelper.Ok_STATUS_CODE_VALUE, content = @Content)
+    @ApiResponse(description = StringHelper.BAD_REQUEST, responseCode = StringHelper.BAD_REQUEST_STATUS_CODE_VALUE, content = @Content)
+    @ApiResponse(description = StringHelper.INTERNAL_SERVER_ERROR, responseCode = StringHelper.INTERNAL_SERVER_ERROR_STATUS_CODE_VALUE, content = @Content)
     public void deletePerson(@PathVariable(value = "id") String id){
         personService.deletePerson(id);
     }
---
+++ src/main/java/br/com/rest/Helper/StringHelper.java
@@ -0,0 +1,16 @@
+package br.com.rest.Helper;
+
+public class StringHelper {
+
+    public static final String Ok = "OK";
+    public static final String Ok_STATUS_CODE_VALUE = "200";
+
+    public static final String CREATED = "Created";
+    public static final String CREATED_STATUS_CODE_VALUE = "201";
+    public static final String BAD_REQUEST = "Bad Request";
+    public static final String BAD_REQUEST_STATUS_CODE_VALUE = "400";
+    public static final String NOT_FOUND = "Not Found";
+    public static final String NOT_FOUND_STATUS_CODE_VALUE = "404";
+    public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
+    public static final String INTERNAL_SERVER_ERROR_STATUS_CODE_VALUE = "500";
+}
